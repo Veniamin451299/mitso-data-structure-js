151620af04b222b165b10a9af0d0247dda0f2223
Veniamin451299 <viip720@gmail.com>
2025-09-24 19:15:41 +0300

    feat: bloom filter completed

--- src/bloom-filter.js
+++ src/bloom-filter.js
@@ -4,26 +4,36 @@ module.exports = class BloomFilter {
   /**
    * @param {number} size - the size of the storage.
    */
-  constructor() {
-    // Bloom filter size directly affects the likelihood of false positives.
-    // The bigger the size the lower the likelihood of false positives.
+  constructor(size = 100) {
+    this.size = size;
+    this.store = this.createStore(this.size);
   }
 
   /**
    * @param {string} item
    */
-  insert(/* item */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  insert(item) {
+    const hashValues = this.getHashValues(item);
+    
+    hashValues.forEach(hash => {
+      this.store.setValue(hash);
+    });
   }
 
   /**
    * @param {string} item
    * @return {boolean}
    */
-  mayContain(/* item */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  mayContain(item) {
+    const hashValues = this.getHashValues(item);
+    
+    for (let i = 0; i < hashValues.length; i++) {
+      if (!this.store.getValue(hashValues[i])) {
+        return false;
+      }
+    }
+    
+    return true;
   }
 
   /**
@@ -35,36 +45,64 @@ module.exports = class BloomFilter {
    * @param {number} size
    * @return {Object}
    */
-  createStore(/* size */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  createStore(size) {
+    const storage = new Array(size).fill(false);
+    
+    return {
+      getValue(index) {
+        return storage[index];
+      },
+      
+      setValue(index) {
+        storage[index] = true;
+      }
+    };
   }
 
   /**
    * @param {string} item
    * @return {number}
    */
-  hash1(/* item */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  hash1(item) {
+    let hash = 0;
+    
+    for (let i = 0; i < item.length; i++) {
+      hash = (hash << 5) + hash + item.charCodeAt(i);
+      hash = hash & hash; // Convert to 32bit integer
+      hash = Math.abs(hash);
+    }
+    
+    return hash % this.size;
   }
 
   /**
    * @param {string} item
    * @return {number}
    */
-  hash2(/* item */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  hash2(item) {
+    let hash = 5381;
+    
+    for (let i = 0; i < item.length; i++) {
+      hash = (hash << 5) + hash + item.charCodeAt(i);
+    }
+    
+    return Math.abs(hash) % this.size;
   }
 
   /**
    * @param {string} item
    * @return {number}
    */
-  hash3(/* item */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  hash3(item) {
+    let hash = 0;
+    
+    for (let i = 0; i < item.length; i++) {
+      hash = (hash << 5) - hash;
+      hash += item.charCodeAt(i);
+      hash = hash & hash; // Convert to 32bit integer
+    }
+    
+    return Math.abs(hash) % this.size;
   }
 
   /**
@@ -73,8 +111,11 @@ module.exports = class BloomFilter {
    * @param {string} item
    * @return {number[]}
    */
-  getHashValues(/* item */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  getHashValues(item) {
+    return [
+      this.hash1(item),
+      this.hash2(item),
+      this.hash3(item)
+    ];
   }
 };
